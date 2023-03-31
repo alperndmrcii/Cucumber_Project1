b75d744e197772e48361753e2181f80281f7291e
Alperen Demirci <alperndmrcii@gmail.com>
2023-03-31 19:38:43 +0300

    Register Steps

---
+++ src/test/java/FeatureFiles/_01_Register.feature
@@ -0,0 +1,7 @@
+Feature: Register to Parabank
+  Scenario: Register Functionality
+    Given Navigate to parabank
+    When Click on Register button
+    And Fill the necessary informations
+    And click on the register button
+    Then Success message should be displayed
---
+++ src/test/java/StepDefinitions/_01_RegisterSteps.java
@@ -0,0 +1,28 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_RegisterSteps {
+    @Given("Navigate to parabank")
+    public void navigateToParabank() {
+    }
+
+    @When("Click on Register button")
+    public void clickOnRegisterButton() {
+    }
+
+    @And("Fill the necessary informations")
+    public void fillTheNecessaryInformations() {
+    }
+
+    @And("click on the register button")
+    public void clickOnTheRegisterButton() {
+    }
+
+    @Then("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+    }
+}
