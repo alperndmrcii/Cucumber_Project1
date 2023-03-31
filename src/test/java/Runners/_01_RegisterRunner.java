e866ed9e85c88dad0dd3a448b8010d67f02dcbb6
Alperen Demirci <alperndmrcii@gmail.com>
2023-03-31 22:13:41 +0300

    test3 passed & Extent report added

---
+++ src/test/java/Runners/_01_RegisterRunner.java
@@ -0,0 +1,8 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/FeatureFiles",glue = {"StepDefinitions"},plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class _01_RegisterRunner extends AbstractTestNGCucumberTests {
+}
